0da8989bc28190b46751a30258a376794a7914b0
lkh <leekh-wm18@student.tarc.edu.my>
2024-09-08 17:22:09 +0800

    Register,login interface done

---
+++ app/src/main/java/com/example/encryptedtrans/data/virustotalanalysisresult.kt
@@ -0,0 +1,17 @@
+package com.example.encryptedtrans.data
+
+import com.google.gson.annotations.SerializedName
+
+data class virustotalanalysisresult(
+    val malicious: Int,
+    val suspicious: Int,
+    val undetected: Int,
+    val harmless: Int,
+    val timeout: Int,
+    @SerializedName("confirmed-timeout")
+    val confirmedTimeout: Int,
+    val failure: Int,
+    @SerializedName("type-unsupported")
+    val typeUnsupported: Int
+)
+
